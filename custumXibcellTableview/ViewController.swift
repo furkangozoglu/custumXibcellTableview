dcb787a61cbbed35a8d06f22dcc2545324f13b8b
farukgozoglu@icloud.com <farukgozoglu@icloud.com>
2019-07-09 21:30:51 -0700

    test

--- custumXibcellTableview/ViewController.swift
+++ custumXibcellTableview/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        func furkan(){
+            print("hello worl44444")
+        }
     }
 
 
